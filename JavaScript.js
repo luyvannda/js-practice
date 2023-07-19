4d8715d8fb8223626705f4168405a367207471ea
luyvannda <philips.joe329@gmail.com>
2023-07-19 15:50:19 +0700

    add a red color text and a blue color heading using DOM manipulation

--- JavaScript.js
+++ JavaScript.js
@@ -2,6 +2,15 @@ const container = document.querySelector("#container");
 
 const content = document.createElement("div");
 content.classList.add("content");
-content.textContent = "This another div added using JavaScript";
-
+content.textContent = "This a new div added using JavaScript";
 container.appendChild(content);
+
+const redPara = document.createElement("p");
+redPara.style.color = 'red';
+redPara.textContent = "Hey I'm red!";
+container.appendChild(redPara);
+
+const blueHeading = document.createElement("h3");
+blueHeading.style.color = 'blue';
+blueHeading.textContent = "I'm a blue h3!";
+container.appendChild(blueHeading);
