904786a3b441df59f825d7b951ba92131b246c6f
luyvannda <philips.joe329@gmail.com>
2023-07-19 16:03:10 +0700

    add a new div with heading and text inside the div before add the div to the container

--- JavaScript.js
+++ JavaScript.js
@@ -14,3 +14,19 @@ const blueHeading = document.createElement("h3");
 blueHeading.style.color = 'blue';
 blueHeading.textContent = "I'm a blue h3!";
 container.appendChild(blueHeading);
+
+const divPink = document.createElement("div");
+divPink.style.backgroundColor = "pink";
+divPink.style.border = "1px, solid, black";
+
+const divHeading = document.createElement("h1");
+divHeading.textContent = "I'm in a div";
+divPink.appendChild(divHeading);
+
+
+const divPara = document.createElement('p');
+divPara.textContent = "ME TOO!";
+divPink.appendChild(divPara);
+
+
+container.appendChild(divPink);
