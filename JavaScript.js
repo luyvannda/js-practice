7e87e02155f5fe39cc18703007bbe9b541a47df0
luyvannda <philips.joe329@gmail.com>
2023-07-20 10:08:45 +0700

    Practice JavaScript event and event listener from the odin projects

--- JavaScript.js
+++ JavaScript.js
@@ -1,32 +1,22 @@
-const container = document.querySelector("#container");
+function alertFunction() {
+  alert("Awesome, you did it!!!");
+}
 
-const content = document.createElement("div");
-content.classList.add("content");
-content.textContent = "This a new div added using JavaScript";
-container.appendChild(content);
+// const btn = document.querySelector("#btn");
+// btn.onclick = alertFunction;
 
-const redPara = document.createElement("p");
-redPara.style.color = 'red';
-redPara.textContent = "Hey I'm red!";
-container.appendChild(redPara);
+// btn.addEventListener('click', alertFunction);
 
-const blueHeading = document.createElement("h3");
-blueHeading.style.color = 'blue';
-blueHeading.textContent = "I'm a blue h3!";
-container.appendChild(blueHeading);
+/* btn.addEventListener('click', function (x) {
+  x.target.style.background = 'blue';
+  x.target.style.color = 'white';
+});
 
-const divPink = document.createElement("div");
-divPink.style.backgroundColor = "pink";
-divPink.style.border = "1px, solid, black";
+*/
+const buttons = document.querySelectorAll('button');
 
-const divHeading = document.createElement("h1");
-divHeading.textContent = "I'm in a div";
-divPink.appendChild(divHeading);
-
-
-const divPara = document.createElement('p');
-divPara.textContent = "ME TOO!";
-divPink.appendChild(divPara);
-
-
-container.appendChild(divPink);
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    alert(button.id);
+  });
+});
