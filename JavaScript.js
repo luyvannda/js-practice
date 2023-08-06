9045e20851ba5bfc2a4a4578225552f215a55b3e
luyvannda <philips.joe329@gmail.com>
2023-08-06 16:09:55 +0700

    create an object with different properties and methods

---
+++ JavaScript.js
@@ -0,0 +1,10 @@
+const person = {
+  name: ["Bob", "Smith"],
+  age: 32,
+  bio() {
+    console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+  },
+  introduceSelf() {
+    console.log(`Hi! I'm ${this.name[0]}.`);
+  },
+};
