76b3f3cefa327354124782cb1d7164ad60087603
luyvannda <philips.joe329@gmail.com>
2023-08-07 15:01:07 +0700

    More practice into objects

--- JavaScript.js
+++ JavaScript.js
@@ -1,10 +1,23 @@
 const person = {
-  name: ["Bob", "Smith"],
+  name: {
+    first: "Bob",
+    last: "Smith"
+  },
   age: 32,
   bio() {
-    console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+    console.log(`${this.name.first} ${this.name["last"]} is ${this.age} years old.`);
   },
   introduceSelf() {
-    console.log(`Hi! I'm ${this.name[0]}.`);
+    console.log(`Hi! I'm ${this.name["first"]}.`);
   },
+};
+const myDataName = "height";
+const myDataValue = "1.75m";
+person[myDataName] = myDataValue;
+
+person.age = 45;
+person["name"]["first"] = "Jason";
+person["eyes"] = "hazel";
+person.farewell = function () {
+  console.log("Bye everybody!");
 };
