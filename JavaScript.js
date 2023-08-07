1cd77ed2f9dcfaf42eccd3592c1afaeca9878f19
luyvannda <philips.joe329@gmail.com>
2023-08-07 16:09:47 +0700

    Learn to create constructor to quickly create objects

--- JavaScript.js
+++ JavaScript.js
@@ -1,23 +1,16 @@
-const person = {
-  name: {
-    first: "Bob",
-    last: "Smith"
-  },
-  age: 32,
-  bio() {
-    console.log(`${this.name.first} ${this.name["last"]} is ${this.age} years old.`);
-  },
-  introduceSelf() {
-    console.log(`Hi! I'm ${this.name["first"]}.`);
-  },
-};
-const myDataName = "height";
-const myDataValue = "1.75m";
-person[myDataName] = myDataValue;
+function Person(name) {
+  this.name = name;
+  this.introduceSelf = function () {
+    console.log(`Hi! I'm ${this.name}.`);
+  };
+}
 
-person.age = 45;
-person["name"]["first"] = "Jason";
-person["eyes"] = "hazel";
-person.farewell = function () {
-  console.log("Bye everybody!");
-};
+const joe = new Person("Joe");
+joe.name;
+joe.introduceSelf();
+
+const jack = new Person("Jack");
+jack.name;
+jack.introduceSelf();
+
+const myNotification = new Notification("Hello!");
