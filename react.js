d4a79a7ec41d212a61172b03f6ffe9bf60224144
luyvannda <philips.joe329@gmail.com>
2023-09-18 14:29:42 +0700

    learn to use React to add an ul with two lists

--- react.js
+++ react.js
@@ -1,4 +1,19 @@
+function MyReact() {
+  return (
+    <ul>
+      <li>This is the first React list</li>
+      <li>I'm the second React list</li>
+    </ul>
+  )
+};
 
+ReactDOM.render(< MyReact />, document.getElementById("root"));
+
+
+// This how we write react with one liner
+// ReactDOM.render(<h1>Hello, everyone!!!</h1>, document.getElementById("root"));
+
+/*
 function MyApp() {
   return <h1>Hello, world!</h1>;
 }
@@ -6,3 +21,4 @@ function MyApp() {
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 root.render(<MyApp />);
+*/
