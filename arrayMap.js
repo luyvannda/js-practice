0e813e9d25e52b0517224377e551b03173958e8d
luyvannda <philips.joe329@gmail.com>
2023-10-13 15:34:03 +0700

    complete challenge 1 & 2 for array.map()

--- arrayMap.js
+++ arrayMap.js
@@ -1,1 +1,42 @@
-console.log("Hello World!!!")
+
+/*
+Challenge 1:
+Given an array of numbers, return an array of each number, squared
+*/
+const nums = [1, 2, 3, 4, 5]
+// -->       [1, 4, 9, 16, 25]
+// Your code here
+
+const map1 = nums.map((num) => num * num);
+// console.log(`Challenge 1 answer is [${map1}]`);
+
+
+/*
+Challenge 2:
+Given an array of strings, return an array where 
+the first letter of each string is capitalized
+*/
+
+const names = ["alice", "bob", "charlie", "danielle"]
+// -->        ["Alice", "Bob", "Charlie", "Danielle"]
+// Your code here
+const newNames = names.map((string) =>
+  string.charAt(0).toUpperCase() + string.slice(1));
+
+// console.log(newNames);
+
+
+/*
+Challenge 3:
+Given an array of strings, return an array of strings that wraps each
+of the original strings in an HTML-like <p></p> tag.
+
+E.g. given: ["Bulbasaur", "Charmander", "Squirtle"]
+return: ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
+*/
+
+const pokemon = ["Bulbasaur", "Charmander", "Squirtle"]
+// -->          ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
+// Your code here
+
+
