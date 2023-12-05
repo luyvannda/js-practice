cb6a5d5c4bbcc35051c753cb5a08b61291f20742
luyvannda <philips.joe329@gmail.com>
2023-12-05 10:52:09 +0700

    copy stylesheet from react course to quickly style the app component

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,17 @@ import './App.scss'
 
 function App() {
 
+  function handleClick() {
+    console.log("I was clicked!")
+  }
+
   return (
     <>
 
+      <div className="container">
+        <img src="https://picsum.photos/640/360" />
+        <button onClick={handleClick}>Click me</button>
+      </div>
     </>
   )
 }
