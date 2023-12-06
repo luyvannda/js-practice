50c156e0506c13de90bf7b995f13bfdb3c6b46b0
luyvannda <philips.joe329@gmail.com>
2023-12-06 15:29:58 +0700

    practice add mouse enter and mouse leave event handler to react

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
 
 import './App.scss'
 
+/**
+     * Challenge: 
+     * Log something to the console when the mouse hovers over the image
+     */
+
 function App() {
 
   function handleClick() {
     console.log("I was clicked!")
   }
 
+  function mouseEnter() {
+    console.log("Mouse is hover over me!")
+  }
+
+  function mouseLeave() {
+    console.log("mouse just left!")
+  }
+
   return (
     <>
 
       <div className="container">
-        <img src="https://picsum.photos/640/360" />
+        <img onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} src="https://picsum.photos/640/360" />
         <button onClick={handleClick}>Click me</button>
       </div>
     </>
