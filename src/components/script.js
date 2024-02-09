7a2bff11d10998cb50f3e9f1dc276cdc61a6ed16
luyvannda <luyvannda@gmail.com>
2024-02-09 10:22:04 +0700

    review array filter method

--- src/components/script.js
+++ src/components/script.js
@@ -66,7 +66,7 @@ const data = [
     publicationDate: "1965-01-01",
     author: "Frank Herbert",
     genres: ["science fiction", "novel", "adventure"],
-    hasMovieAdaptation: true,
+    hasMovieAdaptation: false,
     pages: 658,
     translations: {
       spanish: ""
@@ -270,3 +270,15 @@ const essentialData = books.map((book) => ({
 }));
 
 console.log(essentialData);
+
+const longBooksWithMovie = books
+  .filter((book) => book.pages > 500 && book.hasMovieAdaptation)
+  .map((book) => book.title);
+
+console.log(longBooksWithMovie);
+
+const adventureBooks = books
+  .filter((book) => book.genres.includes("adventure"))
+  .map((book) => book.title);
+
+console.log(adventureBooks);
