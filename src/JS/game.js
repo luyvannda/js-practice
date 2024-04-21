6fe0b59b4bfcb77e0aca510a11436916d61363e9
luyvannda <luyvannda@gmail.com>
2024-04-21 16:00:51 +0700

    practice prototypal inheritance by creating objects constructors for an rpg game

--- src/JS/game.js
+++ src/JS/game.js
@@ -54,7 +54,7 @@ console.log(Object.prototype.hasOwnProperty("valueOf"));
 
 */
 
-// This is to practice prototypal inheritance
+/* This is to practice prototypal inheritance from the odin project
 
 function Person(name) {
   this.name = name;
@@ -87,3 +87,61 @@ player1.sayName();
 player2.sayName();
 player1.getMarker();
 player2.getMarker();
+*/
+
+// Initialize a constructor function for a new Hero
+function Hero(name, level) {
+  this.name = name;
+  this.level = level;
+}
+
+// Add greet method to the Hero prototype
+Hero.prototype.greet = function () {
+  return `${this.name} says hello.`;
+};
+
+// Initialize Warrior constructor
+function Warrior(name, level, weapon) {
+  // Chain constructor with call
+  Hero.call(this, name, level);
+
+  // Add a new property
+  this.weapon = weapon;
+}
+
+function Healer(name, level, spell) {
+  Hero.call(this, name, level);
+
+  this.spell = spell;
+}
+
+Object.setPrototypeOf(Warrior.prototype, Hero.prototype);
+Object.setPrototypeOf(Healer.prototype, Hero.prototype);
+
+Warrior.prototype.attack = function () {
+  return `${this.name} attacks with the ${this.weapon}.`;
+};
+
+Healer.prototype.cast = function () {
+  return `${this.name} casts ${this.spell}.`;
+};
+
+const hero1 = new Warrior("Joe", 1, "sword");
+const hero2 = new Healer("Kanin", 1, "cure");
+
+console.log(hero1);
+console.log(hero2);
+
+let hero1Proto = Object.getPrototypeOf(hero1);
+let hero2Proto = Object.getPrototypeOf(hero2);
+
+console.log(hero1Proto);
+console.log(hero2Proto);
+
+console.log(hero1.attack());
+console.log(hero2.cast());
+
+/*Prototype properties and methods are not automatically linked when we use call() to chain constructors. We will use Object.setPropertyOf() to link the properties in the Hero constructor to the Warrior and Healer constructors, making sure to put it before any additional methods. */
+
+console.log(hero1.greet());
+console.log(hero2.greet());
