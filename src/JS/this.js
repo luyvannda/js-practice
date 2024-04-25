0bc2f79c53c056594842c986be5a41a3909bc48a
luyvannda <luyvannda@gmail.com>
2024-04-25 10:06:38 +0700

    practice using class constructor

--- src/JS/this.js
+++ src/JS/this.js
@@ -12,7 +12,6 @@ calc.increment();
 calc.increment();
 
 // Starting ECMAScript 2015, JavaScript allows to define constructors using class:
-
 class City {
   constructor(name, traveled) {
     this.name = name;
@@ -28,3 +27,24 @@ const paris = new City("Paris", false);
 paris.travel();
 
 console.log(paris.traveled);
+
+class Vehicle {
+  constructor(type, wheelsCount) {
+    if (!(this instanceof Vehicle)) {
+      throw Error("Error: Incorrect invocation");
+    }
+
+    this.type = type;
+    this.wheelsCount = wheelsCount;
+    return this;
+  }
+}
+
+// Constructor invocation
+const car = new Vehicle("car", 4);
+console.log(car.type);
+console.log(car.wheelsCount);
+console.log(car instanceof Vehicle);
+
+// Function invocation. Throws an error.
+const brokenCar = Vehicle("Broken Car", 3);
